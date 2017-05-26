6183c2f6e5751c27b952e8bc8f9b174bbb24a30b
Maciej Zdeb <maciej.zdeb@allegrogroup.com>
2017-05-26 12:04:14 +0200

    Remove deprecated API v1

--- dnsaas/api/urls.py
+++ dnsaas/api/urls.py
@@ -5,6 +5,5 @@ from powerdns.utils import patterns
 
 urlpatterns = patterns(
     '',
-    url(r'', include('dnsaas.api.v1.urls', namespace='default')),
     url(r'^v2/', include('dnsaas.api.v2.urls', namespace='v2')),
 )
